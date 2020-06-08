0cbef6e169c7ce274fb61c390906501439ff91d8
sleintje1994 <32129734+sleintje1994@users.noreply.github.com>
2020-06-08 15:42:39 +0200

    Checklist with styling
    I have added a styling to the checkboxes. If a task is active, the styling is different. When the task is completed, the next task will get the active styling and all other tasks dont.

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -33,6 +33,7 @@ public class MainView extends VerticalLayout {
 
         Checkbox task1 = new Checkbox("you finished setting up your account", true);
         Checkbox task2 = new Checkbox("Create your first blueprint", false);
+        task2.addClassName("active");
         Checkbox task3 = new Checkbox("Create an import mapping", false);
         Checkbox task4 = new Checkbox("Import your product data", false);
 
@@ -45,23 +46,31 @@ public class MainView extends VerticalLayout {
             if (task1.isEmpty()){
                 task1.setValue(true);
                 task1.setLabel("you finished setting up your account");
+                task2.addClassName("active");
+                task1.removeClassName("active");
             } else {
                 if (task2.isEmpty()){
                     task2.setValue(true);
                     task2.setLabel("You have created your blueprint");
+                    task3.addClassName("active");
+                    task2.removeClassName("active");
                 } else {
                     if (task3.isEmpty()){
                         task3.setValue(true);
                         task3.setLabel("You have created an import mapping");
+                        task4.addClassName("active");
+                        task3.removeClassName("active");
                     } else {
                         if (task4.isEmpty()){
                             task4.setValue(true);
                             task4.setLabel("you have imported your product data");
+                            task4.removeClassName("active");
 
                             addButton.setText("reset list");
                         } else if(task1.isEnabled() | task2.isEnabled() | task3.isEnabled() | task4.isEnabled()){
                             task1.setValue(false);
                             task1.setLabel("Create your account");
+                            task1.addClassName("active");
                             task2.setValue(false);
                             task2.setLabel("Create your first blueprint");
                             task3.setValue(false);
