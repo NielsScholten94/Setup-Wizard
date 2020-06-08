e8c37246e24d953ab14f89a8f0fcdf1f30a1c960
sleintje1994 <32129734+sleintje1994@users.noreply.github.com>
2020-06-08 15:03:40 +0200

    Interactive checklist
    Made an interactieve checklist with if statements when the button is clicked. If the first task is checked, the next task will get checked, and so on untill every task is completed. I also added a reset button.

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -1,12 +1,15 @@
 package com.example;
 
 import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import com.helger.commons.functional.IFunction;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.accordion.Accordion;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -26,25 +29,51 @@ import java.io.FileReader;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        VerticalLayout todosList = new VerticalLayout(); // (1)
+        VerticalLayout todosList = new VerticalLayout();
 
-        Checkbox task1 = new Checkbox("Create account");
-        Checkbox task2 = new Checkbox("Create blueprint");
-        Checkbox task3 = new Checkbox("Create import mapping");
-        Checkbox task4 = new Checkbox("Import product data");
-
-        task1.setValue(true);
-        task2.setValue(false);
-        task3.setValue(false);
-        task4.setValue(false);
+        Checkbox task1 = new Checkbox("you finished setting up your account", true);
+        Checkbox task2 = new Checkbox("Create your first blueprint", false);
+        Checkbox task3 = new Checkbox("Create an import mapping", false);
+        Checkbox task4 = new Checkbox("Import your product data", false);
 
         todosList.add(task1, task2, task3, task4);
 
-        Button addButton = new Button("next task"); // (3)
+        Button addButton = new Button("next task");
         addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        addButton.addClickShortcut(Key.ENTER);
         addButton.addClickListener(click -> {
-            // (4)
+
+            if (task1.isEmpty()){
+                task1.setValue(true);
+                task1.setLabel("you finished setting up your account");
+            } else {
+                if (task2.isEmpty()){
+                    task2.setValue(true);
+                    task2.setLabel("You have created your blueprint");
+                } else {
+                    if (task3.isEmpty()){
+                        task3.setValue(true);
+                        task3.setLabel("You have created an import mapping");
+                    } else {
+                        if (task4.isEmpty()){
+                            task4.setValue(true);
+                            task4.setLabel("you have imported your product data");
+
+                            addButton.setText("reset list");
+                        } else if(task1.isEnabled() | task2.isEnabled() | task3.isEnabled() | task4.isEnabled()){
+                            task1.setValue(false);
+                            task1.setLabel("Create your account");
+                            task2.setValue(false);
+                            task2.setLabel("Create your first blueprint");
+                            task3.setValue(false);
+                            task3.setLabel("Create an import mapping");
+                            task4.setValue(false);
+                            task4.setLabel("Import your product data");
+
+                            addButton.setText("next task");
+                        }
+                    }
+                }
+            }
 
         });
         add( // (5)
