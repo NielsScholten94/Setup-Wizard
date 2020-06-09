406fa9ffcd01e776e0a0ff8e31924047dd731ce2
sleintje1994 <32129734+sleintje1994@users.noreply.github.com>
2020-06-09 14:45:01 +0200

    Added styling and components
    Styling and extra Vaadin components have been added to complete the layout of the setup wizard

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -42,158 +42,242 @@ public class MainView extends VerticalLayout {
 
     public MainView () {
 
+        // create variables to define status of tasks ( this should be pulled and updated from a database )
         Text task1Status = new Text("completed");
         Text task2Status = new Text("active");
         Text task3Status = new Text("open");
         Text task4Status = new Text("open");
 
-        Checkbox row1 = new Checkbox("", false);
-        Button task1Button = new Button("complete task");
-        Checkbox row2 = new Checkbox("", false);
-        Button task2Button = new Button("complete task");
-        Checkbox row3 = new Checkbox("", false);
-        Button task3Button = new Button("complete task");
-        Checkbox row4 = new Checkbox("", false);
-        Button task4Button = new Button("complete task");
+        // create task rows
 
+        // task row 1 (checkbox)
+        Checkbox row1 = new Checkbox("", false);
+
+        // task row 1 (buttons)
+        Button task1Button1 = new Button("Create an account");
+        task1Button1.addClassName("taskbutton");
+
+        HorizontalLayout buttonsTask1 = new HorizontalLayout();
+        buttonsTask1.addClassName("buttonrow");
+        buttonsTask1.add(task1Button1);
+
+        // task row 1 (combined object)
+        VerticalLayout taskrow1 = new VerticalLayout();
+        taskrow1.add(row1, buttonsTask1);
+        taskrow1.addClassName("taskrow");
+
+
+        // task row 2 (checkbox)
+        Checkbox row2 = new Checkbox("", false);
+
+        // task row 2 (buttons)
+        Button task2Button1 = new Button("Use a template");
+        task2Button1.addClassName("taskbutton");
+
+        Button task2Button2 = new Button("Import my own");
+        task2Button2.addClassName("taskbutton");
+
+        HorizontalLayout buttonsTask2 = new HorizontalLayout();
+        buttonsTask2.addClassName("buttonrow");
+        buttonsTask2.add(task2Button1, task2Button2);
+
+        // task row 1 (combined object)
+        VerticalLayout taskrow2 = new VerticalLayout();
+        taskrow2.add(row2, buttonsTask2);
+        taskrow2.addClassName("taskrow");
+
+
+        // task row 3 (checkbox)
+        Checkbox row3 = new Checkbox("", false);
+
+        // task row 3 (buttons)
+        Button task3Button1 = new Button("Create with your data");
+        task3Button1.addClassName("taskbutton");
+
+        Button task3Button2 = new Button("Use demo data");
+        task3Button2.addClassName("taskbutton");
+
+        HorizontalLayout buttonsTask3 = new HorizontalLayout();
+        buttonsTask3.addClassName("buttonrow");
+        buttonsTask3.add(task3Button1, task3Button2);
+
+        // task row 3 (combined object)
+        VerticalLayout taskrow3 = new VerticalLayout();
+        taskrow3.add(row3, buttonsTask3);
+        taskrow3.addClassName("taskrow");
+
+
+        // task row 4 (checkbox)
+        Checkbox row4 = new Checkbox("", false);
+
+        // task row 4 (buttons)
+        Button task4Button1 = new Button("Run the import");
+        task4Button1.addClassName("taskbutton");
+
+        HorizontalLayout buttonsTask4 = new HorizontalLayout();
+        buttonsTask4.addClassName("buttonrow");
+        buttonsTask4.add(task4Button1);
+
+        // task row 4 (combined object)
+        VerticalLayout taskrow4 = new VerticalLayout();
+        taskrow4.add(row4, buttonsTask4);
+        taskrow4.addClassName("taskrow");
+
+
+        // define start position of task elements based on variable task status from database
         if (task1Status.getText().equals("completed")){
             row1.setValue(true);
             row1.setLabel("You finished setting up your account");
-            task1Button.addClassName("disabled");
+            buttonsTask1.addClassName("disabled");
         } else {
             if (task1Status.getText().equals("active")){
                 row1.setValue(false);
                 row1.setLabel("Set up your account");
-                row1.addClassName("active");
+                taskrow1.addClassName("active");
             } else {
                 row1.setValue(false);
                 row1.setLabel("Set up your account");
-                task1Button.addClassName("disabled");
+                buttonsTask1.addClassName("disabled");
             }
         }
 
         if (task2Status.getText().equals("completed")){
             row2.setValue(true);
             row2.setLabel("You have created your first blueprint");
-            task2Button.addClassName("disabled");
+            buttonsTask2.addClassName("disabled");
         } else {
             if (task2Status.getText().equals("active")){
                 row2.setValue(false);
                 row2.setLabel("Create your first blueprint");
-                row2.addClassName("active");
+                taskrow2.addClassName("active");
             } else {
                 row2.setValue(false);
                 row2.setLabel("Create your first blueprint");
-                task2Button.addClassName("disabled");
+                buttonsTask2.addClassName("disabled");
             }
         }
 
         if (task3Status.getText().equals("completed")){
             row3.setValue(true);
             row3.setLabel("You have created an import mapping");
-            task3Button.addClassName("disabled");
+            buttonsTask3.addClassName("disabled");
         } else {
             if (task3Status.getText().equals("active")){
                 row3.setValue(false);
                 row3.setLabel("Create an import mapping");
-                row3.addClassName("active");
+                taskrow3.addClassName("active");
             } else {
                 row3.setValue(false);
                 row3.setLabel("Create an import mapping");
-                task3Button.addClassName("disabled");
+                buttonsTask3.addClassName("disabled");
             }
         }
 
         if (task4Status.getText().equals("completed")){
             row4.setValue(true);
             row4.setLabel("You have imported your product data");
-            task4Button.addClassName("disabled");
+            buttonsTask4.addClassName("disabled");
         } else {
             if (task4Status.getText().equals("active")){
                 row4.setValue(false);
                 row4.setLabel("Import your product data");
-                row4.addClassName("active");
+                taskrow4.addClassName("active");
             } else {
                 row4.setValue(false);
                 row4.setLabel("Import your product data");
-                task4Button.addClassName("disabled");
+                buttonsTask4.addClassName("disabled");
             }
         }
 
-        task1Button.addClickListener(click -> {
+        // define action when task is completed
+        task1Button1.addClickListener(click -> {
 
             row1.setValue(true);
             row1.setLabel("You finished setting up your account");
-            task1Button.addClassName("disabled");
-            row1.removeClassName("active");
-            row2.addClassName("active");
-            task2Button.removeClassName("disabled");
+            buttonsTask1.addClassName("disabled");
+            taskrow1.removeClassName("active");
+            taskrow2.addClassName("active");
+            buttonsTask2.removeClassName("disabled");
 
         });
 
-        task2Button.addClickListener(click -> {
+        task2Button1.addClickListener(click -> {
 
             row2.setValue(true);
             row2.setLabel("You have created your first blueprint");
-            task2Button.addClassName("disabled");
-            row2.removeClassName("active");
-            row3.addClassName("active");
-            task3Button.removeClassName("disabled");
+            buttonsTask2.addClassName("disabled");
+            taskrow2.removeClassName("active");
+            taskrow3.addClassName("active");
+            buttonsTask3.removeClassName("disabled");
 
         });
 
-        task3Button.addClickListener(click -> {
+        task3Button1.addClickListener(click -> {
 
             row3.setValue(true);
             row3.setLabel("You have created an import mapping");
-            task3Button.addClassName("disabled");
-            row3.removeClassName("active");
-            row4.addClassName("active");
-            task4Button.removeClassName("disabled");
+            buttonsTask3.addClassName("disabled");
+            taskrow3.removeClassName("active");
+            taskrow4.addClassName("active");
+            buttonsTask4.removeClassName("disabled");
 
         });
 
-        task4Button.addClickListener(click -> {
+        task4Button1.addClickListener(click -> {
 
             row4.setValue(true);
             row4.setLabel("You have imported your product data");
-            task4Button.addClassName("disabled");
-            row4.removeClassName("active");
+            buttonsTask4.addClassName("disabled");
+            taskrow4.removeClassName("active");
 
         });
 
+        // create page layout
         VerticalLayout todosList = new VerticalLayout();
-        todosList.add(row1, task1Button, row2, task2Button, row3, task3Button, row4, task4Button);
+        todosList.addClassName("noPadding");
+        todosList.add(taskrow1, taskrow2, taskrow3, taskrow4);
 
+        H1 title = new H1("How to get started with Beeyond?");
+
+        VerticalLayout content = new VerticalLayout();
+        content.addClassName("noPadding");
+        content.add(title, todosList);
+
+        // reset button
         Button resetButton = new Button("reset list");
         resetButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         resetButton.addClickListener(click -> {
 
             row1.setValue(false);
             row1.setLabel("Set up your account");
-            row1.addClassName("active");
-            task1Button.removeClassName("disabled");
+            taskrow1.addClassName("active");
+            buttonsTask1.removeClassName("disabled");
 
             row2.setValue(false);
             row2.setLabel("Create your first blueprint");
-            row2.removeClassName("active");
-            task2Button.addClassName("disabled");
+            taskrow2.removeClassName("active");
+            buttonsTask2.addClassName("disabled");
 
             row3.setValue(false);
             row3.setLabel("Create an import mapping");
-            row3.removeClassName("active");
-            task3Button.addClassName("disabled");
+            taskrow3.removeClassName("active");
+            buttonsTask3.addClassName("disabled");
 
             row4.setValue(false);
             row4.setLabel("Import your product data");
-            row4.removeClassName("active");
-            task4Button.addClassName("disabled");
+            taskrow4.removeClassName("active");
+            buttonsTask4.addClassName("disabled");
 
         });
+
+        // page layout
+        VerticalLayout page = new VerticalLayout();
+        page.add(content, resetButton);
+        page.addClassName("page");
+
+        // execute content on the page
         add(
-                new H1("How to get started with Beeyond?"),
-                todosList,
-                resetButton
+                page
         );
 
 
